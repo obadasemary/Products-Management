4c0ff6c7c538c1b2445a7a99f1e328b8fb4e667a
عبدالرحمن السمري <obada.semary@gmail.com>
2015-06-09 16:35:04 +0200

    update 1.1
    DataAccessLayer
    Inisialize the connection object
    Method to open the connection
    Method to close the connection
    Method To Read Data From Database
    Method to insert , update , delete Data from Database

---
+++ Products Management/DAL/DataAccessLayer.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace Products_Management.DAL
+{
+    class DataAccessLayer
+    {
+        SqlConnection sqlconnection;
+
+        //This Constructor Inisialize the connection object
+        public DataAccessLayer()
+        {
+            sqlconnection = new SqlConnection(@"Server=.\OBADA; Database=Product_DB; Integrated Security=true");
+        }
+
+        //Method to open the connection
+        public void Open()
+        {
+            if (sqlconnection.State != ConnectionState.Open)
+            {
+                sqlconnection.Open();
+            }
+        }
+
+        //Method to close the connection 
+        public void Close()
+        {
+            if (sqlconnection.State == ConnectionState.Open)
+            {
+                sqlconnection.Close();
+            }
+        }
+
+        //Method To Read Data From Database
+        public DataTable SelectData(string stored_procedure, SqlParameter[] param)
+        {
+            SqlCommand sqlcmd = new SqlCommand();
+            sqlcmd.CommandType = CommandType.StoredProcedure;
+            sqlcmd.CommandText = stored_procedure;
+
+            if (param != null)
+            {
+                for (int i = 0; i < param.Length; i++)
+                {
+                    sqlcmd.Parameters.Add(param[i]);
+                }
+            }
+            SqlDataAdapter da = new SqlDataAdapter(sqlcmd);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            return dt;
+        }
+
+        //Method to insert , update , delete Data from Database
+        public void ExecuteCommand(string stored_procedure, SqlParameter[] param)
+        {
+            SqlCommand sqlcmd = new SqlCommand();
+            sqlcmd.CommandType = CommandType.StoredProcedure;
+            sqlcmd.CommandText = stored_procedure;
+
+            if (param != null)
+            {
+                sqlcmd.Parameters.AddRange(param);
+            }
+            sqlcmd.ExecuteNonQuery();
+        }
+    }
+}
